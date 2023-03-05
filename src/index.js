dc5b83d5fe014e4feb14b352efc2c9489c2d1aa0
kirhal <kirillgaleckiy@gmail.com>
2023-03-05 15:30:18 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
+Notify.init({
+  fontSize: '20px',
+  width: '400px',
+  position: 'center-center',
+});
 
 const DEBOUNCE_DELAY = 300;
+
+
+
+
+
+
+
+
+
+
+
+
+// function onSubmitForm(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { delay, step, amount },
+//   } = event.currentTarget;
+//   for (let i = 0; i < Number(amount.value); i += 1) {
+//     createPromise(i + 1, Number(delay.value) + Number(step.value) * i)
+//       .then(({ position, delay }) => {
+//         Notify.success(`✔ Fulfilled promise ${position} in ${delay}ms`);
+//       })
+//       .catch(({ position, delay }) => {
+//         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//       });
+//   }
+// }
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+// }
