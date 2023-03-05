04decd40c79e5b49175c3f2a551c6b812b5f8baf
kirhal <kirillgaleckiy@gmail.com>
2023-03-05 17:25:26 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -19,11 +19,14 @@ function fetchCountries(name) {
     .then(r => {
       return r.json();
     })
-    .then(data => onDataReceive(data));
+    .then(data => onDataReceive(data))
+    .catch(error => {
+      Notify.failure('❌ Oops, there is no country with that name');
+    });
 }
 
 function onInputData(e) {
-  let name = e.target.value.trim();
+  const name = e.target.value.trim();
   fetchCountries(name);
 }
 function onDataReceive(data) {
@@ -33,31 +36,3 @@ function onDataReceive(data) {
     Notify.success('From 2 to 10');
   } else Notify.success('111111111111');
 }
-
-// function onSubmitForm(event) {
-//   event.preventDefault();
-//   const {
-//     elements: { delay, step, amount },
-//   } = event.currentTarget;
-//   for (let i = 0; i < Number(amount.value); i += 1) {
-//     createPromise(i + 1, Number(delay.value) + Number(step.value) * i)
-//       .then(({ position, delay }) => {
-//         Notify.success(`✔ Fulfilled promise ${position} in ${delay}ms`);
-//       })
-//       .catch(({ position, delay }) => {
-//         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//       });
-//   }
-// }
-// function createPromise(position, delay) {
-//   return new Promise((resolve, reject) => {
-//     const shouldResolve = Math.random() > 0.3;
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         resolve({ position, delay });
-//       } else {
-//         reject({ position, delay });
-//       }
-//     }, delay);
-//   });
-// }
