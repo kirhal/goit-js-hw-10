0cd73f33ef5ec41b406ca9523dc03a6e50a94ac5
kirhal <kirillgaleckiy@gmail.com>
2023-03-05 17:20:07 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 Notify.init({
   fontSize: '20px',
   width: '400px',
@@ -7,17 +8,31 @@ Notify.init({
 });
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('#search-box');
+const url = `https://restcountries.com/v2/name/`;
+const urlOpt = `?fields=name,capital,population,flag,languages`;
 
+inputEl.addEventListener('input', debounce(onInputData, DEBOUNCE_DELAY));
 
+function fetchCountries(name) {
+  fetch(`${url}${name}${urlOpt}`)
+    .then(r => {
+      return r.json();
+    })
+    .then(data => onDataReceive(data));
+}
 
-
-
-
-
-
-
-
-
+function onInputData(e) {
+  let name = e.target.value.trim();
+  fetchCountries(name);
+}
+function onDataReceive(data) {
+  if (data.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  } else if (2 <= data.length && data.length <= 10) {
+    Notify.success('From 2 to 10');
+  } else Notify.success('111111111111');
+}
 
 // function onSubmitForm(event) {
 //   event.preventDefault();
