401a00ca395b4acb7444c9ce4f8cd726477075a8
kirhal <kirillgaleckiy@gmail.com>
2023-03-06 17:00:27 +0200

    2 function imports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { listMarkUp, countryMarkUp, clearAllMarkUp } from './markUp';
 import { fetchCountries } from './fetchCountries';
 import './css/styles.css';
 import debounce from 'lodash.debounce';
@@ -10,8 +11,6 @@ Notify.init({
 
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
-const listEl = document.querySelector('.country-list');
-const countryEl = document.querySelector('.country-info');
 
 inputEl.addEventListener('input', debounce(onInputData, DEBOUNCE_DELAY));
 
@@ -30,44 +29,10 @@ function onInputData(e) {
       clearAllMarkUp();
     });
 }
-
 function dataReceive(obj) {
   if (obj.length > 10) {
     Notify.info('Too many matches found. Please enter a more specific name.');
   } else if (2 <= obj.length && obj.length <= 10) {
     listMarkUp(obj);
   } else countryMarkUp(obj[0]);
-}
-function listMarkUp(countries) {
-  countryEl.innerHTML = '';
-  const markUpData = countries
-    .map(
-      ({ flags: { svg }, name }) =>
-        `<li><img src=${svg} alt="flag of ${name}" width="70">
-          <span style="font-weight: medium;font-size: 25px;"> ${name}</span></li>`
-    )
-    .join('');
-
-  listEl.innerHTML = markUpData;
-}
-function countryMarkUp({
-  name,
-  capital,
-  population,
-  flags: { svg },
-  languages,
-}) {
-  listEl.innerHTML = '';
-  const countryLanguages = languages.map(el => el.name).join(', ');
-  const markUpData = `<h1 style="font-size:45px;">
-  <img src=${svg} alt="flag of ${name}" width="70"> ${name}</h1>
-  <li><span style="font-weight: bold;font-size: 25px;">Capital: </span><span style="font-size: 25px;">${capital}</span></li>
-  <li><span style="font-weight: bold;font-size: 25px;">Population: </span><span style="font-size: 25px;">${population}</span></li>
-  <li><span style="font-weight: bold;font-size: 25px;">Languages: </span><span style="font-size: 25px;">${countryLanguages}</span></li>`;
-
-  countryEl.innerHTML = markUpData;
-}
-function clearAllMarkUp() {
-  countryEl.innerHTML = '';
-  listEl.innerHTML = '';
 }
